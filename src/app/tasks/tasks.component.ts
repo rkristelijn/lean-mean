2a881d4d7472b93489b66663ade4ef20be2eafb8
Remi Kristelijn <rkristelijn@gmail.com>
2017-10-13 10:41:15 +0000

    improved layout

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -7,31 +7,56 @@ import { TaskService } from '../task.service';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks:any[];
+  tasks: any[];
+  public alerts: any = [];
   constructor(private ts: TaskService) { }
+
   ngOnInit() {
     this.getTasks();
   }
   getTasks() {
     this.ts.list().subscribe(data => {
       this.tasks = data;
+      this.addAlertInfo('Tasks loaded.'); 
     });
   }
   update(task: any) {
-    if(task.name !== task.newName) {
-      this.ts.update(task).subscribe(data => {
-        this.getTasks();
-      });
+    if (task.name !== task.newName) {
+      this.ts.update(task).subscribe(
+        data => {
+          this.getTasks();
+          this.addAlertSuccess('Update completed.');
+        },
+        e => {
+          console.error(e);
+          this.addAlertDanger(e._body);
+        },
+      );
     }
   }
   delete(task: any) {
     this.ts.delete(task).subscribe(data => {
       this.getTasks();
+      this.addAlertSuccess('Delete completed.');
     });
   }
   create(name: string) {
     this.ts.create(name).subscribe(data => {
       this.getTasks();
+      this.addAlertSuccess('Create completed.');
     });
+  }
+
+  public addAlert(alert: any): void {
+    this.alerts.push(alert);
+  }
+  public addAlertInfo(msg: string) {
+    this.addAlert({ type: 'info', msg: msg, timeout: 2000 });
+  }
+  public addAlertSuccess(msg: string) {
+    this.addAlert({ type: 'success', msg: msg, timeout: 2000 });
+  }
+  public addAlertDanger(msg: string) {
+    this.addAlert({ type: 'danger', msg: msg, dismissible: true });
   }
 }
