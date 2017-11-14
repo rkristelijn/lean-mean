19992605e698a8634690703c771412c0eae897d2
Remi Kristelijn <rkristelijn@gmail.com>
2017-11-14 16:15:23 +0000

    test push

--- server.js
+++ server.js
@@ -4,24 +4,24 @@ var express = require('express'),
   port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/task/model'),
-  bodyParser = require('body-parser');
-  
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Tododb', { useMongoClient: true }); 
+  bodyParser = require('body-parser')
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/Tododb', { useMongoClient: true })
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-var routes = require('./api/task/route');
+app.use(express.static(path.join(__dirname, 'dist')))
 
-routes(app);
+var routes = require('./api/task/route')
+
+routes(app)
 
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'dist/index.html'));
 // });
 
-app.listen(port);
+app.listen(port)
 
-console.log('task list RESTful API server started on: ' + port);
+console.log('task list RESTful API server started on: ' + port)
